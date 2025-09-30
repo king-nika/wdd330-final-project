a552d71cc031dba75e6c057e909c8bda92f90383
ak-nika <akingbayiojo@gmail.com>
2025-09-30 21:25:28 +0100

    added animation to top

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,8 @@ loadHeaderAndFooter();
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 const tmdb = new TMDB(apiKey);
 const results = await tmdb.getTrendingMovies();
-console.log(results);
 
-const displayResults = async (movies) => {
+const displayMovies = async (movies) => {
   const template = document.querySelector("template");
   const moviesContainer = document.getElementById("movies-container");
 
@@ -33,11 +32,54 @@ const displayResults = async (movies) => {
       : "N/A";
     p.textContent = `${releaseYear} | ${genreNames.join(", ")}`;
 
-    container.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`;
+    container.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
     h1.textContent = movie.title;
 
     moviesContainer.appendChild(clone);
   });
 };
 
-displayResults(results);
+const displayRandomMovie = async (movies) => {
+  if (!movies || movies.length === 0) {
+    return;
+  }
+
+  const genresArray = await tmdb.getGenres();
+  const genreMap = Object.fromEntries(genresArray.map((g) => [g.id, g.name]));
+
+  const randomIndex = Math.floor(Math.random() * movies.length);
+  const randomMovie = movies[randomIndex];
+  const genreNames = randomMovie.genre_ids.map((id) => genreMap[id]);
+
+  const randomContainer = document.getElementById("random-movie");
+  const img = randomContainer.querySelector("img");
+  const h1 = randomContainer.querySelector("h1");
+  const p = randomContainer.querySelector("p");
+
+  const releaseYear = randomMovie.release_date
+    ? new Date(randomMovie.release_date).getFullYear()
+    : "N/A";
+
+  img.src = `https://image.tmdb.org/t/p/original${randomMovie.backdrop_path}`;
+  h1.textContent = randomMovie.title;
+  p.innerHTML = `${releaseYear} | ${genreNames.join(
+    ", "
+  )} | <span class="flex items-center gap-1"><img src="/assets/star.svg" class="size-5" alt="Rating" /> ${randomMovie.vote_average.toFixed(
+    1
+  )}</span>`;
+};
+
+displayMovies(results);
+displayRandomMovie(results);
+setInterval(() => {
+  const randomContainer = document.getElementById("random-movie");
+  randomContainer.querySelector("#animation").style.animationPlayState =
+    "paused";
+  randomContainer.querySelector("img").style.animationPlayState = "paused";
+
+  displayRandomMovie(results);
+
+  randomContainer.querySelector("img").style.animationPlayState = "running";
+  randomContainer.querySelector("#animation").style.animationPlayState =
+    "running";
+}, 3000);
